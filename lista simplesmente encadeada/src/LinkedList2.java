7403b430c34698c1d8e1df72b52923cfdbc889f3
Brunobetiatto <127573399+Brunobetiatto@users.noreply.github.com>
2024-09-02 12:10:46 -0300

    att

--- lista simplesmente encadeada/src/LinkedList2.java
+++ lista simplesmente encadeada/src/LinkedList2.java
@@ -1,63 +1,132 @@
-class LinkedList2 {
-    class No {
-        int dado;
-        No proximo;
+class Node {
+    int data;
+    Node next;
 
-        public No(int dado) {
-            this.dado = dado;
-            this.proximo = null;
+    public Node(int data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class ListaSimplesmenteEncadeada {
+    private Node head;
+    private Node tail;
+
+    public ListaSimplesmenteEncadeada() {
+        this.head = null;
+        this.tail = null;
+    }
+
+    public boolean vazia() {
+        return head == null;
+    }
+
+    public void insere_primeiro(int info) {
+        Node newNode = new Node(info);
+        if (vazia()) {
+            head = tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
         }
     }
 
-    private No comeco;
-    private No fim;
-    private int tamanho;
-
-    public LinkedList2() {
-        this.comeco = null;
-        this.fim = null;
-        this.tamanho = 0;
+    public void insere_depois(Node no, int info) {
+        if (no == null) {
+            throw new IllegalArgumentException("Nó não pode ser nulo");
+        }
+        Node newNode = new Node(info);
+        newNode.next = no.next;
+        no.next = newNode;
+        if (no == tail) {
+            tail = newNode;
+        }
     }
 
-    // Método para inserir um elemento de forma ordenada
-    public void inserirOrdenado(int dado) {
-        No novoNo = new No(dado);
+    public void insere_ultimo(int info) {
+        Node newNode = new Node(info);
+        if (vazia()) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
 
-        // a) Lista está vazia
-        if (comeco == null) {
-            comeco = novoNo;
-            fim = novoNo;
-        }
-        // b) Elemento é menor que o primeiro
-        else if (dado < comeco.dado) {
-            novoNo.proximo = comeco;
-            comeco = novoNo;
-        }
-        // c) Elemento é maior que o último
-        else if (dado > fim.dado) {
-            fim.proximo = novoNo;
-            fim = novoNo;
-        }
-        // d) Elemento é intermediário
-        else {
-            No atual = comeco;
-            while (atual.proximo != null && atual.proximo.dado < dado) {
-                atual = atual.proximo;
+    public void insere_ordenado(int info) {
+        Node newNode = new Node(info);
+        if (vazia() || head.data >= info) {
+            insere_primeiro(info);
+        } else {
+            Node current = head;
+            while (current.next != null && current.next.data < info) {
+                current = current.next;
             }
-            novoNo.proximo = atual.proximo;
-            atual.proximo = novoNo;
+            newNode.next = current.next;
+            current.next = newNode;
+            if (newNode.next == null) {
+                tail = newNode;
+            }
         }
-
-        tamanho++;
     }
 
-    // Método para imprimir todos os elementos da lista
-    public void imprimirLista() {
-        No atual = comeco;
-        while (atual != null) {
-            System.out.print(atual.dado + " ");
-            atual = atual.proximo;
+    public void mostra_lista() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " ");
+            current = current.next;
         }
         System.out.println();
+    }
+
+    public int retira_primeiro() {
+        if (vazia()) {
+            throw new RuntimeException("A lista está vazia");
+        }
+        int data = head.data;
+        head = head.next;
+        if (head == null) {
+            tail = null;
+        }
+        return data;
+    }
+
+    public int retira_ultimo() {
+        if (vazia()) {
+            throw new RuntimeException("A lista está vazia");
+        }
+        if (head == tail) { // Apenas um elemento
+            return retira_primeiro();
+        }
+        Node current = head;
+        while (current.next != tail) {
+            current = current.next;
+        }
+        int data = tail.data;
+        tail = current;
+        tail.next = null;
+        return data;
+    }
+
+    public int retira_depois(Node no) {
+        if (no == null || no.next == null) {
+            throw new IllegalArgumentException("Nó inválido ou último nó");
+        }
+        int data = no.next.data;
+        no.next = no.next.next;
+        if (no.next == null) {
+            tail = no;
+        }
+        return data;
+    }
+
+    public int ultimo_elemento() {
+        if (vazia()) {
+            throw new RuntimeException("A lista está vazia");
+        }
+        return tail.data;
+    }
+    public Node getHead() {
+        return head;
     }
 }
--- lista simplesmente encadeada/src/Main.java
+++ lista simplesmente encadeada/src/Main.java
@@ -14,16 +14,17 @@ public class Main {
         lista.imprimirLista();  // Saída: 10 20 30 40
 
         // Criando e usando a segunda lista
-        LinkedList2 lista2 = new LinkedList2();
+        ListaSimplesmenteEncadeada lista2 = new ListaSimplesmenteEncadeada();
 
         // Inserindo elementos de forma ordenada
-        lista2.inserirOrdenado(20);
-        lista2.inserirOrdenado(10);
-        lista2.inserirOrdenado(30);
-        lista2.inserirOrdenado(25);
+        lista2.insere_ordenado(20);
+        lista2.insere_ordenado(10);
+        lista2.insere_ordenado(30);
+        lista2.insere_ordenado(25);
+        lista2.insere_depois(lista2.getHead().next, 15);
 
         // Imprimindo os elementos da lista ordenada
         System.out.print("LinkedList2: ");
-        lista2.imprimirLista();  // Saída: 10 20 25 30
+        lista2.mostra_lista();  // Saída: 10 20 25 30
     }
 }
